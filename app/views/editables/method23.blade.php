71c50e045bf40a8e2a5fda47b6b8ca1c27b22f8e
catskay <catskay@gmail.com>
2014-11-16 19:24:51 -0500

    edit

--- app/views/editables/method23.blade.php
+++ app/views/editables/method23.blade.php
@@ -21,11 +21,12 @@
 
     <div class="panel panel-default">
         <div class="panel body">
-            {{Form::open()}}
-            {{Form::select('choose', array('general' => 'General', 'yogic' => 'Yogic'), 'HigherSelf', array('onchange' => 'change()', 'id' => 'options'));}}
+            {{Form::open(array('id' => 'optionSelection'))}}
+            {{Form::select('choose', array('neutral' => 'Neutral','general' => 'General', 'yogic' => 'Yogic'), 'neutral', array('onchange' => 'change()', 'id' => 'options'));}}
             {{Form::close()}}
 
 <p>
+    Choose one of the techniques below that best suits your audience. <br/>
     Incorporate a few breaths of silence between each line or two, with a space of 8-10 breaths at the deepest point of your integration.
 </p>
             <p id="contents">
@@ -47,11 +48,12 @@
     </div>
     <script>
     function change(){
+        if(document.getElementById("options").value == 'neutral'){
+            document.getElementById("contents").innerHTML = "·          Now allow yourself to enter the deepest state of letting go right now. <br/> ·          Nothing to do or achieve.<br/> ·          Open..empty.<br/> ·          (pause)<br/> ·          Feel yourself as a vast, spacious, sky of awareness.<br/> ·          Rest into and as this space.<br/> ·          (pause)<br/> ·          Thoughts, images, sensations passing by like clouds on a clear blue sky.<br/> ·          Be the space in which it is all happening. <br/>"; }
         if(document.getElementById("options").value == 'yogic'){
-document.getElementById("contents").innerHTML = "";
-}
+            document.getElementById("contents").innerHTML = "-  Let your attention shift, and focus your inward gaze on the Third Eye and remain focused and steady in Ajna chakra consciousness.<br/> -   You have entered the sacred space of Shiva consciousness, where unmanifest potentiality has become manifest actuality........<br/> -   Establish yourself in the unmanifest part of your being.<br/> -   And allow the duality of mind to merge into the oneness of spirit.<br/> -   Settle into the silent source of your being.<br/> -   Let go in the experience so totally that you become the experience.<br/> -   Now bring your attention to back Ajna chakra, the command center.<br/> -   Feel the energy of Ida and Pingala, Shiva and Shakti, merging into the unified state of Oneness...<br/> -   Bring your attention to the pineal gland and ativate the elixir of immortality...Amrita.<br/> -   Feel as it floods your entire nervous system, bathing every cell in the luminous, healing light of divine Presence.<br/>"; }
         if(document.getElementById("options").value == 'general'){
-            document.getElementById("contents").innerHTML = "          Now allow yourself to enter the deepest state of letting go right now. <br/> ·          Nothing to do or achieve.<br/> ·          Open..empty.<br/> ·          (pause)<br/> ·          Feel yourself as a vast, spacious, sky of awareness.<br/> ·          Rest into and as this space.<br/> ·          (pause)<br/> ·          Thoughts, images, sensations passing by like clouds on a clear blue sky.<br/> ·          Be the space in which it is all happening. <br/>"; }
+            document.getElementById("contents").innerHTML = "         - Bring your attention to the center between your eyebrows and drop into the deepest level of relaxation...<br/> -   Here, there remains nothing to do or achieve, you have entered the domain of grace.<br/> -   In this domain of integration, you are witness of all that is happening but doing nothing to make it happen.<br/> -   All that can never be done by your doing can happen only in the non-doing presence of your being.<br/> -   Feel completely safe and comfortable as you hand yourself over to the power and protection of the Presence.<br/> -   Feel it.... experience it....be it....<br/> -   Hand over all fear, apprehension and anxiety about all that you want to change control and manage ....let go of all doing...<br/> -   Replace it with trust and faith in the Higher Power.<br/> -   Let your mind merge and melt into the presence and enter the sanctuary of silence.<br/> -   Remain empty and free from all doing.<br/> -   Feel yourself as time-transcendent presence, right now......<br/> "; }
    }
     </script>
 
--- app/views/editables/method24.blade.php
+++ app/views/editables/method24.blade.php
@@ -13,44 +13,51 @@
 
     <div class="row">
         <div class="col-lg-12">
-                <p><b> {{$array['section']->sname}} </b></p>
-                <pIndent1>{{$array['subsection']->ssname}}</pIndent1>
-                <pIndent2>{{$array['method']->mname}} </pIndent2>
+            <p><b> {{$array['section']->sname}} </b></p>
+            <pIndent1>{{$array['subsection']->ssname}}</pIndent1>
+            <pIndent2>{{$array['method']->mname}} </pIndent2>
         </div>
     </div>
 
     <div class="panel panel-default">
         <div class="panel body">
             {{Form::open()}}
-            {{Form::select('choose', array('general' => 'General', 'yogic' => 'Yogic'), 'HigherSelf', array('onchange' => 'change()', 'id' => 'options'));}}
+            {{Form::select('choose', array('general' => 'General', 'yogic' => 'Yogic'), 'general', array('onchange' => 'change()', 'id' => 'options'));}}
             {{Form::close()}}
 
-            <p id="contents">
-·          Now allow yourself to enter the deepest state of letting go right now. <br/>
-·          Nothing to do or achieve.<br/>
-·          Open..empty.<br/>
-·          (pause)<br/>
-·          Feel yourself as a vast, spacious, sky of awareness.<br/>
-·          Rest into and as this space.<br/>
-·          (pause)<br/>
-·          Thoughts, images, sensations passing by like clouds on a clear blue sky.<br/>
-·          Be the space in which it is all happening. <br/>
-
+            <p id="content1">
+                -   Here your intention and your affirmations are actualized and fulfilled with effortless ease.
             </p>
-           
-
+            -   Now bring your intention into your awareness… <br/>
+            {{Form::open(array('id' => 'formText'))}}
+            {{ Form::textarea('notes', null, ['size' => '80x3', 'placeholder' => 'Please enter your custom intention here.']) }}
+            {{Form::close()}}
+            <p id="content2">
+                -   Repeat your intention now three times..........(pause)<br/>
+                -   If you do not have an intention, just remain silent and let spirit, who knows what is best, do it for you...<br/>
+                -   If you have self-defeating patterns or habits that are holding you back that you want to be freed from, make that your intention now.......(allow time).<br/>
+                -   Allow it to go to the deepest levels of recognition with no hesitation.<br/>
+                -   Know that your higher self recognizes, honors and accepts your intention.<br/>
+                -   Have faith and trust that it has been heard and is being acted upon by a higher power of the source within you.<br/>
+                -   There is no need for you to do anything about it...<br/>
+                -   Bring your attention back to the center between the eyebrows and feel all the energies of your body from all meridians activated, purified and balanced.<br/>
+                -   All channels are open and the life force is flowing freely, healing the body and calming the mind...open your heart and feel content...<br/>
+                -   Just experience (....pause...).<br/>
+            </p>
 
         </div>
     </div>
     <script>
     function change(){
         if(document.getElementById("options").value == 'yogic'){
-document.getElementById("contents").innerHTML = "";
-}
+            document.getElementById("content1").innerHTML = "-  Go deeper into the Third Eye, the mysterious command center and let it carry out your intentions, prayers and affirmations.";
+            document.getElementById("content2").innerHTML = "-  Hold your intention firmly and clearly with deep feeling and faith.<br/> -   Establish it at the altar of your heart.<br/> -   Repeat your intention with the conviction of it being received by the Sadguru that guides you from darkness to light.<br/> -   Know that you are established in that part of your being where your intentions are actualized and fulfilled with effortless ease.<br/> -   In the Third Eye you are empowered to create what you pray for, visualize, and affirm.<br/> -   Your prayers and intentions are carried out at the deepest core of your being.<br/> -   Feel your intention is now being assimilated and adopted by all levels of your body and your being.<br/> -   Completely integrated, totally digested and fully actualized.<br/> ";
+        }
         if(document.getElementById("options").value == 'general'){
-            document.getElementById("contents").innerHTML = "";
+            document.getElementById("content1").innerHTML = "-   Here your intention and your affirmations are actualized and fulfilled with effortless ease. ";
+            document.getElementById("content2").innerHTML = " -   Repeat your intention now three times..........(pause)<br/> -   If you do not have an intention, just remain silent and let spirit, who knows what is best, do it for you...<br/> -   If you have self-defeating patterns or habits that are holding you back that you want to be freed from, make that your intention now.......(allow time).<br/> -   Allow it to go to the deepest levels of recognition with no hesitation.<br/> -   Know that your higher self recognizes, honors and accepts your intention.<br/> -   Have faith and trust that it has been heard and is being acted upon by a higher power of the source within you.<br/> -   There is no need for you to do anything about it...<br/> -   Bring your attention back to the center between the eyebrows and feel all the energies of your body from all meridians activated, purified and balanced.<br/> -   All channels are open and the life force is flowing freely, healing the body and calming the mind...open your heart and feel content...<br/> -   Just experience (....pause...).<br/>";
+        }
     }
-   }
     </script>
 
     <div class="row">
